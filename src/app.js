b9a30d1aede094ac5ee75ec1bb7a4cd082e45fd3
Akshay Gaur <akshay.gaur028@gmail.com>
2025-08-24 15:57:54 +0530

    basic apis added

--- src/app.js
+++ src/app.js
@@ -19,6 +19,59 @@ app.post("/signup", async(req,res)=>{
     }
 });
 
+//get user by email
+app.get("/user", async (req,res)=>{
+  const userEmail = req.body.email;
+  
+  try {
+    const users = await User.find({email: userEmail});
+
+    if(users.length===0){
+      res.status(404).send("User not found")
+    }
+    else{
+      res.send(users)
+    }
+
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Feed api => Get all users from the database
+app.get("/feed", async (req,res)=>{
+  try {
+    const users = await User.find({})
+    res.send(users)
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+})
+
+app.delete("/user", async (req,res)=>{
+  const userId = req.body.userId;
+
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted Successfully")
+    console.log("delete ho gaya")
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+})
+
+app.patch("/user", async (req,res)=>{
+  const userId = req.body.userId;
+  const data = req.body;
+
+  try {
+    await User.findByIdAndUpdate({_id: userId},data)
+    res.send("User updated successfully")
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+})
+
 connectDB()
   .then(() => {
     console.log("Database connection established...");
