1c67a3b45b46edaf5e81185a556e0a3fbed54a3c
Akshay Gaur <akshay.gaur028@gmail.com>
2025-09-15 23:22:12 +0530

    validation done using validator library

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator")
 
 const userSchema = new mongoose.Schema(
   {
@@ -15,10 +16,20 @@ const userSchema = new mongoose.Schema(
       unique: true,
       lowercase: true,
       trim: true,
+      validate(value){
+        if(!validator.isEmail(value)){
+          throw new Error("Invalid email address " + value)
+        }
+      }
     },
     password: {
       type: String,
       required: true,
+      validate(value){
+        if(!validator.isStrongPassword(value)){
+          throw new Error("Enter a strong password " + value)
+        }
+      }
     },
     age: {
       type: Number,
@@ -36,6 +47,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       default:
         "https://ctmirror.org/wp-content/uploads/2021/01/dummy-man-570x570-1.png",
+      validate(value){
+        if(!validator.isURL(value)){
+          throw new Error("Invalid Photo URL " + value)
+        }
+      }
     },
     about: {
       type: String,
