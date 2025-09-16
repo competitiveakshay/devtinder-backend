060ba1449050cc26ddd3fd795ef3a9a1d1078abd
Akshay Gaur <akshay.gaur028@gmail.com>
2025-09-16 23:26:28 +0530

    Encryption done using bcrypt

--- src/app.js
+++ src/app.js
@@ -2,21 +2,59 @@ const express = require('express');
 const connectDB = require("./config/database");
 const app = express();
 const User = require('./models/user');
+const {validateSignUpData} = require('./utils/validation')
+const bcrypt = require("bcrypt")
 
 app.use(express.json());
 
 app.post("/signup", async(req,res)=>{
-
-    //creating a new instance of User model
-    const user = new User(req.body);
-
     try{
+        // Validation of data
+        validateSignUpData(req);
+
+        const {firstName, lastName, email, password} = req.body;
+
+        // Encrypt the password
+        const hashPassword = await bcrypt.hash(password,10);
+        console.log(hashPassword);
+
+        //creating a new instance of User model
+        const user = new User({
+          firstName,
+          lastName,
+          email,
+          password: hashPassword
+        });
+
         await user.save();
         res.send("User Added Successfully!");
     }
     catch(err){
-        res.status(400).send("Error saving the data:" + err.message);
+        res.status(400).send("Error: " + err.message);
     }
+});
+
+app.post("/login", async (req,res)=>{
+  try {
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email:email});
+
+    if(!user){
+      throw new Error("Login Failed");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(isPasswordValid){
+      res.send("Login Successful");
+    }
+    else{
+      throw new Error("Login Failed");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
 });
 
 //get user by email
